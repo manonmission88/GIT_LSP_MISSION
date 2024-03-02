24b246b855ac81c3e28a18f926ebe9798b3d35f6
manonmission88 <manishnewray88@gmail.com>
2024-03-01 22:59:56 -0500

    Assignment4 creating set operation

---
+++ large_scale_programming/src/org/howard/edu/lsp/assignment4/IntegerSetException.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment4;
+
+/**
+ * {@summary class to throw IntegerSetException}
+ * @author Manish Niure 
+ */
+public class IntegerSetException extends Exception {
+	private static final long serialVersionUID = 1L;
+    public IntegerSetException(String message) {
+        super(message);
+    }
+}
